88c5f8ea3a4264ddd532007c36cb32801682ed79
rosie-s <rosie-s@users.noreply.github.com>
2018-02-05 22:12:19 +0000

    feat: Solution for difference between dates

--- Udacity/02_IntroToComputerScience/Code/Lesson3/Lesson3.py
+++ Udacity/02_IntroToComputerScience/Code/Lesson3/Lesson3.py
@@ -8,30 +8,47 @@
 # Given your birthday and the current date, calculate your age in days.
 # Account for leap days.
 
-###
-# Step 1
-###
+
+# Define a procedure to check for leap year
+def is_leap_year(year):
+    if year % 400 == 0:
+        return True
+    if year % 100 == 0:
+        return False
+    if year % 4 == 0:
+        return True
+    return False
+
+
+# Define a procedure to get days in months
+def days_in_month(year, month):
+    # if month in (1,3,5,7,8,10,12)
+    if month == 1 or month == 3 or month == 5 or month == 7 \
+            or month == 8 or month == 10 or month == 12:
+        return 31
+    else:
+        if month == 2:
+            if is_leap_year(year):
+                return 29
+            else:
+                return 28
+        else:
+            return 30
+
+
 # Define a simple nextDay procedure, that assumes every month has 30 days.
-
-
 def next_day(year, month, day):
     """
     Warning: Returns the year, month, day of the next day.
     Simple version: assume every month has 30 days.
     """
-    if day < 30:
+    if day < days_in_month(year, month):
         return year, month, day + 1
     else:
         if month < 12:
             return year, month + 1, 1
         else:
             return year + 1, 1, 1
-
-
-next_String = "Next day for"
-print(next_String, '(1999,12,30) is', next_day(1999, 12, 30))  # => (2000, 1, 1)
-print(next_String, '(1999,12,30) is', next_day(2013, 1, 30))  # => (2013, 2, 1)
-print(next_String, '(1999,12,30) is', next_day(2012, 12, 30))  # => (2013, 1, 1)
 
 
 def date_is_before(year1, month1, day1, year2, month2, day2):
@@ -61,7 +78,37 @@ def days_between_dates(year1, month1, day1, year2, month2, day2):
     return days
 
 
+def my_test():
+    # Test next_Day
+    next_string = "Next day for"
+    print(next_string, '(1999,12,30) is', next_day(1999, 12, 30))  # => (2000, 1, 1)
+    print(next_string, '(1999,12,30) is', next_day(2013, 1, 30))  # => (2013, 2, 1)
+    print(next_string, '(1999,12,30) is', next_day(2012, 12, 30))  # => (2013, 1, 1)
+
+    # Test with 30-day months!
+    assert days_between_dates(2013, 1, 1, 2013, 1, 1) == 0
+    assert days_between_dates(2013, 1, 1, 2013, 1, 2) == 1
+    assert next_day(2013, 1, 1) == (2013, 1, 2)
+    assert next_day(2013, 4, 30) == (2013, 5, 1)
+    assert next_day(2012, 12, 31) == (2013, 1, 1)
+    print("Test 30 days: Test Finished!")
+
+    # Test with non leap years
+    assert next_day(2013, 2, 28) == (2013, 3, 1)
+    assert next_day(2013, 9, 30) == (2013, 10, 1)
+    assert days_between_dates(2013, 1, 1, 2014, 1, 1) == 365
+    print("Test with non leap year: Test Finished!")
+
+    # Test with leap years
+    assert next_day(2012, 2, 28) == (2012, 2, 29)
+    assert days_between_dates(2012, 1, 1, 2013, 1, 1) == 366
+    assert days_between_dates(2013, 1, 1, 2014, 1, 1) == 365
+    assert days_between_dates(2013, 1, 24, 2013, 6, 29) == 156
+    print("Test with leap year: Test Finished!")
+
+
 def test():
+    # Original test cases
     test_cases = [((2012, 1, 1, 2012, 2, 28), 58),
                   ((2012, 1, 1, 2012, 3, 1), 60),
                   ((2011, 6, 30, 2012, 6, 30), 366),
@@ -77,3 +124,4 @@ def test():
 
 
 test()
+my_test()
