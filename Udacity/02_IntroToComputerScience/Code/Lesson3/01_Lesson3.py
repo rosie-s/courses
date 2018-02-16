741092a96da38063715e57eabb1e2e8a9a70f06e
rosie-s <rosie-s@users.noreply.github.com>
2018-02-16 22:31:51 +0000

    feat: Add measure_udacity procedure

--- Udacity/02_IntroToComputerScience/Code/Lesson3/01_Lesson3.py
+++ Udacity/02_IntroToComputerScience/Code/Lesson3/01_Lesson3.py
@@ -185,3 +185,22 @@ def sum_list(s):
 print(sum_list([1, 7, 4]))  # 12
 print(sum_list([9, 4, 10]))  # 23
 print(sum_list([44, 14, 76]))  # 134
+
+
+###
+# Measure Udacity
+###
+# Define a procedure, measure_udacity, that takes as its input a list of strings,
+# and returns a number that is a count of the number of elements in the input
+# list that start with the uppercase letter 'U'.
+
+def measure_udacity(l):
+    count = 0
+    for e in l:
+        if e[0] == 'U':
+            count = count + 1
+    return count
+
+
+print(measure_udacity(['Dave', 'Sebastian', 'Katy']))  # 0
+print(measure_udacity(['Umika', 'Umberto']))  # 2
