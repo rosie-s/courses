7afa0f682e46905ecfe82fd2a97adfc8d54a3fc7
rosie-s <rosie-s@users.noreply.github.com>
2018-03-06 20:26:48 +0000

    style: Change comments layout

--- Udacity/02_IntroToComputerScience/Code/Lesson4/01_Lesson4.py
+++ Udacity/02_IntroToComputerScience/Code/Lesson4/01_Lesson4.py
@@ -7,9 +7,7 @@
 # Add to Index
 ###
 # Define a procedure, add_to_index, that takes 3 inputs:
-# - an index: [[<keyword>,[<url>,...]],...]
-# - a keyword: String
-# - a url: String
+# - an index: [[<keyword>,[<url>,...]],...] - a keyword: String - a url: String
 # If the keyword is already in the index, add the url to the list of urls associated with that keyword.
 # If the keyword is not in the index, add an entry to the index: [keyword,[url]]
 def add_to_index(index, keyword, url):
@@ -33,8 +31,7 @@ print("Index is:\n", ind,
 # Lookup
 ###
 # Define a procedure, lookup, that takes two inputs:
-# - an index
-#  - keyword
+# - an index - keyword
 # The procedure should return a list of the urls associated with the keyword. If the keyword
 # is not in the index, the procedure should return an empty list.
 def lookup(index, keyword):
