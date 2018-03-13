464e6265a7616ad0ac90777d4a4a4bdbd3dc775e
rosie-s <rosie-s@users.noreply.github.com>
2018-03-13 22:48:53 +0000

    style: Modify comment layout

--- Udacity/02_IntroToComputerScience/Code/Lesson4/ProblemSet.py
+++ Udacity/02_IntroToComputerScience/Code/Lesson4/ProblemSet.py
@@ -7,11 +7,9 @@
 ###
 # The built-in <string>.split() procedure works okay, but fails to find all the words on a page because it only uses
 # whitespace to split the string. To do better, we should also use punctuation marks to split the page into words.
-# Define a procedure, split_string, that takes two inputs: the string to split and a string containing all of the
-# characters considered separators. The procedure should return a list of strings that break the source string up by
-# the characters in the splitlist.
-
-
+# Define a procedure, split_string, that takes two inputs: 
+# the string to split and a string containing all of the characters considered separators. 
+# The procedure should return a list of strings that break the source string up by the characters in the splitlist.
 def split_string(source, splitlist):
     output = []
     at_split = True  # at a split point?
