5cda9c9439b765e48ab5d10ce439aae6d0c21a24
rosie-s <rosie-s@users.noreply.github.com>
2018-03-15 21:39:47 +0000

    style: Modify comment layout

--- Udacity/02_IntroToComputerScience/Code/Lesson4/ProblemSet.py
+++ Udacity/02_IntroToComputerScience/Code/Lesson4/ProblemSet.py
@@ -45,17 +45,12 @@ print(splitString, ",\t", out)  # ['First Name', 'Last Name', 'Street Address', 
 ###
 # Improving the Index
 ###
-# The current index includes a url in the list of urls
-# for a keyword multiple times if the keyword appears
+# The current index includes a url in the list of urls for a keyword multiple times if the keyword appears
 # on that page more than once.
-
-# It might be better to only include the same url
-# once in the url list for a keyword, even if it appears
+# It might be better to only include the same url once in the url list for a keyword, even if it appears
 # many times.
-
-# Modify add_to_index so that a given url is only
-# included once in the url list for a keyword,
-# no matter how many times that keyword appears.
+# Modify add_to_index so that a given url is only included once in the url list for a keyword, no matter 
+# how many times that keyword appears.
 def add_to_index(index, keyword, url):
     for entry in index:
         if entry[0] == keyword:
