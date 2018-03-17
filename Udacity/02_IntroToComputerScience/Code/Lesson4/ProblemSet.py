aa645dbbaede01f689f9d1f7d37732b2e5ab2297
rosie-s <rosie-s@users.noreply.github.com>
2018-03-17 19:16:40 +0000

    style: Modify comments layout

--- Udacity/02_IntroToComputerScience/Code/Lesson4/ProblemSet.py
+++ Udacity/02_IntroToComputerScience/Code/Lesson4/ProblemSet.py
@@ -152,44 +152,19 @@ print("Look for 'is':", lookup(index, "is"))  # ['http://www.udacity.com/cs101x/
 ###
 # Counting Clicks
 ###
-
-# One way search engines rank pages
-# is to count the number of times a
-# searcher clicks on a returned link.
-# This indicates that the person doing
-# the query thought this was a useful
-# link for the query, so it should be
+# One way search engines rank pages is to count the number of times a searcher clicks on a returned link.
+# This indicates that the person doing the query thought this was a useful link for the query, so it should be
 # higher in the rankings next time.
-
-# (In Unit 6, we will look at a different
-# way of ranking pages that does not depend
-# on user clicks.)
-
-# Modify the index such that for each url in a
-# list for a keyword, there is also a number
-# that counts the number of times a user
-# clicks on that link for this keyword.
-
-# The result of lookup(index,keyword) should
-# now be a list of url entries, where each url
-# entry is a list of a url and a number
-# indicating the number of times that url
-# was clicked for this query keyword.
-
-# You should define a new procedure to simulate
-# user clicks for a given link:
-
-# record_user_click(index,word,url)
-
-# that modifies the entry in the index for
-# the input word by increasing the count associated
-# with the url by 1.
-
-# You also will have to modify add_to_index
-# in order to correctly create the new data
-# structure, and to prevent the repetition of
-# entries as in homework 4-5.
-
+# 
+# Modify the index such that for each url in a list for a keyword, there is also a number that counts the number 
+# of times a user clicks on that link for this keyword.
+# The result of lookup(index,keyword) should now be a list of url entries, where each urL entry is a list of a url 
+# and a number indicating the number of times that url was clicked for this query keyword.
+#
+# You should define a new procedure to simulate user clicks for a given link: record_user_click(index,word,url)
+# that modifies the entry in the index for the input word by increasing the count associated with the url by 1.
+# You also will have to modify add_to_index in order to correctly create the new data structure, and to prevent 
+# the repetition of entries as in homework 4-5.
 def record_user_click(index, keyword, url):
     urls = lookup(index, keyword)
     if urls:
@@ -296,18 +271,17 @@ def lookup(index, keyword):
 print("\n***Counting Clicks***")
 index = crawl_web('http://www.udacity.com/cs101x/index.html')
 print("Look for 'is':", lookup(index, 'good'))
-# >>> [['http://www.udacity.com/cs101x/index.html', 0],
-# >>> ['http://www.udacity.com/cs101x/crawling.html', 0]]
+# [['http://www.udacity.com/cs101x/index.html', 0],
+# ['http://www.udacity.com/cs101x/crawling.html', 0]]
 record_user_click(index, 'good', 'http://www.udacity.com/cs101x/crawling.html')
 print("Look for 'is':", lookup(index, 'good'))
-# >>> [['http://www.udacity.com/cs101x/index.html', 0],
-# >>> ['http://www.udacity.com/cs101x/crawling.html', 1]]
+# [['http://www.udacity.com/cs101x/index.html', 0],
+# ['http://www.udacity.com/cs101x/crawling.html', 1]]
 
 
 ###
 # Time Spent at Routers
 ###
-
 total_time = 75  # ms traceroute, roundtrip Birmingham -> Sundsvall
 one_Way_distance = 2500.0  # km, Birmingham -> Sundsvall
 optic_speed = 200000  # km/s
