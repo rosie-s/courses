7e7eb018e1a114c53a6978aa6d9041e8788c5b61
rosie-s <rosie-s@users.noreply.github.com>
2018-03-23 19:51:08 +0000

    style: Modify comment layout

--- Udacity/02_IntroToComputerScience/Code/Lesson4/ProblemSet.py
+++ Udacity/02_IntroToComputerScience/Code/Lesson4/ProblemSet.py
@@ -253,19 +253,12 @@ print("speed_fraction(50,10000):", speed_fraction(50, 10000))  # 1.33333333333
 ###
 # Converting Seconds
 ###
-# Write a procedure, convert_seconds, which takes as input a non-negative
-# number of seconds and returns a string of the form
-# '<integer> hours, <integer> minutes, <number> seconds' but
-# where if <integer> is 1 for the number of hours or minutes,
-# then it should be hour/minute. Further, <number> may be an integer
-# or decimal, and if it is 1, then it should be followed by second.
-# You might need to use int() to turn a decimal into a float depending
-# on how you code this. int(3.0) gives 3
+# Write a procedure, convert_seconds, which takes as input a non-negative number of seconds and returns a string of the form
+# '<integer> hours, <integer> minutes, <number> seconds' but where if <integer> is 1 for the number of hours or minutes,
+# then it should be hour/minute. Further, <number> may be an integer or decimal, and if it is 1, then it should be followed
+# by second. You might need to use int() to turn a decimal into a float depending on how you code this. int(3.0) gives 3
 #
-# Note that English uses the plural when talking about 0 items, so
-# it should be "0 minutes".
-#
-
+# Note that English uses the plural when talking about 0 items, soit should be "0 minutes".
 def convert_seconds(seconds):
     minutes = int(seconds / 60)
     seconds -= minutes * 60
