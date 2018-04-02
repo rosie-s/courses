36ae3c3efbdcd7a019126eedf62288f4210a84d9
rosie-s <rosie-s@users.noreply.github.com>
2018-04-02 14:53:01 +0100

    fear: Add code for Make Big Index

--- Udacity/02_IntroToComputerScience/Code/Lesson5/01_Lesson5.py
+++ Udacity/02_IntroToComputerScience/Code/Lesson5/01_Lesson5.py
@@ -36,3 +36,56 @@ print("Time Execution: spin_loop(100000)   :", time_execution('spin_loop(100000)
 print("Time Execution: spin_loop(1000000)  :", time_execution('spin_loop(1000000)'))
 print("Time Execution: spin_loop(10000000) :", time_execution('spin_loop(10**7)')[1])
 
+
+###
+# Make Big Index
+###
+
+# Lookup
+def lookup(index, keyword):
+    for e in index:
+        if e[0] == keyword:
+            return e[1]
+    return []
+
+
+# Add to Index
+def add_to_index(index, keyword, url):
+    for e in index:
+        if e[0] == keyword:
+            e[1].append(url)
+            return
+    index.append([keyword, [url]])
+
+
+# Make String
+def make_string(p):
+    s = ""
+    for e in p:
+        s = s + e
+    return s
+
+
+def make_big_index(size):
+    index = []
+    letters = ['a', 'a', 'a', 'a', 'a', 'a', 'a', 'a']
+    while len(index) < size:
+        word = make_string(letters)
+        add_to_index(index, word, 'fake')
+        for i in range(len(letters) - 1, 0, -1):
+            if letters[i] < 'z':
+                letters[i] = chr(ord(letters[i]) + 1)
+                break
+            else:
+                letters[i] = 'a'
+    return index
+
+
+print("\n***Make Big Index***")
+s = "Make Big Index:"
+print(s, 3, " :", make_big_index(3))
+print(s, 10, ":", make_big_index(10))
+index1000 = make_big_index(1000)
+print("Time Execution: 'lookup(index1000, udacity)' :", time_execution('lookup(index1000, "udacity")'))
+print("Index 999 :", index1000[999])
+print("Index  -1 :", index1000[-1])
