60c1c83566bb1c17071a26b3a116212a9b858718
rosie-s <rosie-s@users.noreply.github.com>
2018-04-15 20:30:03 +0100

    feat: Add code for Finding the Right Bucket

--- Udacity/02_IntroToComputerScience/Code/Lesson5/01_Lesson5.py
+++ Udacity/02_IntroToComputerScience/Code/Lesson5/01_Lesson5.py
@@ -200,3 +200,29 @@ def make_hashtable(nbuckets):
 print("\n*Empty Hash Table*")
 print("Empty Table (3):", make_hashtable_while(3))
 print("Empty Table (3):", make_hashtable(3))
+
+
+###
+# Finding the Right Bucket
+###
+# Define a procedure, hashtable_get_bucket,
+# that takes two inputs - a hashtable, and
+# a keyword, and returns the bucket where the
+# keyword could occur.
+def hashtable_get_bucket(htable, keyword):
+    return htable[hash_string(keyword, len(htable))]
+
+
+table = [[['Francis', 13], ['Ellis', 11]], [], [['Bill', 17], ['Zoe', 14]], [['Coach', 4]],
+         [['Louis', 29], ['Rochelle', 4], ['Nick', 2]]]
+
+print("\n*Finding the Right Bucket*")
+
+print("Bucket - Zoe:", hashtable_get_bucket(table, "Zoe"))
+# >>> [['Bill', 17], ['Zoe', 14]]
+
+print("Bucket - Brick:", hashtable_get_bucket(table, "Brick"))
+# >>> []
+
+print("Bucket - Lilith:", hashtable_get_bucket(table, "Lilith"))
+# >>> [['Louis', 29], ['Rochelle', 4], ['Nick', 2]]
