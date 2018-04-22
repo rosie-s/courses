3bef2f6c5c80d91d7ebfcdd6e7b868b4fc83ed3f
rosie-s <rosie-s@users.noreply.github.com>
2018-04-22 21:05:23 +0100

    style: Update comment layout

--- Udacity/02_IntroToComputerScience/Code/Lesson5/01_Lesson5.py
+++ Udacity/02_IntroToComputerScience/Code/Lesson5/01_Lesson5.py
@@ -220,14 +220,8 @@ print("Lilith:", hashtable_get_bucket(table, "Lilith"))  # [['Louis', 29], ['Roc
 ###
 # Adding Keywords
 ###
-# Define a procedure,
-#
-#    hashtable_add(htable,key,value)
-#
-# that adds the key to the hashtable (in
-# the correct bucket), with the correct
-# value and returns the new hashtable.
-
+# Define a procedure,hashtable_add(htable,key,value) that adds the key to the hashtable (in the correct bucket), 
+# with the correct value and returns the new hashtable.
 def hashtable_add(htable, key, value):
     bucket = hashtable_get_bucket(htable, key)
     bucket.append([key, value])
@@ -246,5 +240,4 @@ hashtable_add(table, 'Zoe', 14)
 
 print("\n*Adding Keywords*")
 print("Table: ", table)
-# [[['Ellis', 11], ['Francis', 13]], [], [['Bill', 17], ['Zoe', 14]],
-# [['Coach', 4]], [['Louis', 29], ['Nick', 2], ['Rochelle', 4]]]
+# [[['Ellis', 11], ['Francis', 13]], [], [['Bill', 17], ['Zoe', 14]], [['Coach', 4]], [['Louis', 29], ['Nick', 2], ['Rochelle', 4]]]
