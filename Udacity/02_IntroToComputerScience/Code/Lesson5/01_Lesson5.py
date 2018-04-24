49024a9ea305cb521d3c8bb38bdb206cc6cd58dc
rosie-s <rosie-s@users.noreply.github.com>
2018-04-24 12:57:44 +0100

    style: Modify layout

--- Udacity/02_IntroToComputerScience/Code/Lesson5/01_Lesson5.py
+++ Udacity/02_IntroToComputerScience/Code/Lesson5/01_Lesson5.py
@@ -246,12 +246,8 @@ print("Table: ", table)
 ###
 # Lookup
 ###
-# Define a procedure,
-# hashtable_lookup(htable,key)
-# that takes two inputs, a hashtable
-# and a key (string),
-# and returns the value associated
-# with that key.
+# Define a procedure, hashtable_lookup(htable,key), that takes two inputs, a hashtable and a key (string),
+# and returns the value associated with that key.
 def hashtable_lookup(htable, key):
     bucket = hashtable_get_bucket(htable, key)
     for entry in bucket:
@@ -260,29 +256,16 @@ def hashtable_lookup(htable, key):
     return None
 
 print("\n*Lookup*")
-
-print("Lookup: Francis -", hashtable_lookup(table, 'Francis'))
-#>>> 13
-
-print ("Lookup: Louis   -", hashtable_lookup(table, 'Louis'))
-#>>> 29
-
-print ("Lookup: Zoe     -", hashtable_lookup(table, 'Zoe'))
-#>>> 14
+print("Lookup: Francis -", hashtable_lookup(table, 'Francis')) # 13
+print ("Lookup: Louis   -", hashtable_lookup(table, 'Louis')) # 29
+print ("Lookup: Zoe     -", hashtable_lookup(table, 'Zoe')) # 14
 
 
 ###
 # Update
 ###
-# Define a procedure,
-# hashtable_update(htable,key,value)
-# that updates the value associated with key. If key is already in the
-# table, change the value to the new value. Otherwise, add a new entry
-# for the key and value.
-
-# Hint: Use hashtable_lookup as a starting point.
-# Make sure that you return the new htable
-
+# Define a procedure,hashtable_update(htable,key,value) that updates the value associated with key. If key is already in the
+# table, change the value to the new value. Otherwise, add a new entry for the key and value.
 def hashtable_update(htable,key,value):
     bucket = hashtable_get_bucket(htable, key)
     for entry in bucket:
@@ -298,6 +281,5 @@ hashtable_update(table, 'Bill', 42)
 hashtable_update(table, 'Rochelle', 94)
 hashtable_update(table, 'Zed', 68)
 print("Updated table: \n", table)
-#>>> [[['Ellis', 11], ['Francis', 13]], [['Zed', 68]], [['Bill', 42],
-#>>> ['Zoe', 14]], [['Coach', 4]], [['Louis', 29], ['Nick', 2],
-#>>> ['Rochelle', 94]]]
+# [[['Ellis', 11], ['Francis', 13]], [['Zed', 68]], [['Bill', 42],
+# ['Zoe', 14]], [['Coach', 4]], [['Louis', 29], ['Nick', 2], ['Rochelle', 94]]]
