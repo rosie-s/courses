bf131fcc3461be7ec69fe80554f51c86958db715
rosie-s <rosie-s@users.noreply.github.com>
2019-11-24 00:11:32 +0100

    refactor: Use Unreal naming convention

--- Udemy/01_UnrealEngineC++Developer/Section2_TripleX/TripleX.cpp
+++ Udemy/01_UnrealEngineC++Developer/Section2_TripleX/TripleX.cpp
@@ -5,20 +5,22 @@ int main()
     // Print welcome messages to the terminal
     std::cout << "You are a secret agent breaking into a secure server room....";
     std::cout << std::endl;
-    std::cout << "Enter the correct code to continue";
+    std::cout << "Enter the correct code to continue" << std::endl;
 
-    // Declare 3 number code
-    const int a = 1;
-    const int b = 2;
-    const int c = 3;
+    const int CodeA = 1;
+    const int CodeB = 2;
+    const int CodeC = 3;
 
-    const int sum = a + b + c;
-    const int product = a * b * c;
+    const int CodeSum = CodeA + CodeB + CodeC;
+    const int CodeProduct = CodeA * CodeB * CodeC;
 
     // Print sum and product to the terminal
     std::cout << std::endl;
-    std::cout << sum << std::endl;
-    std::cout << product << std::endl;
+    std::cout << "** There are 3 numbers in the code" << std::endl;
+    std::cout << "** The codes add up to: " << CodeSum << std::endl;
+    std::cout << "** The codes multiply to give: " << CodeProduct << std::endl;
 
+    int PlayerGuess;
+    
     return 0;
 }
