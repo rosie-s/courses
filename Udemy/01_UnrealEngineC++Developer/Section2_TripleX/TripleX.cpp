18f25be4574626c6c8e4cbedbe78566442940b8f
rosie-s <rosie-s@users.noreply.github.com>
2019-11-23 22:49:19 +0100

    feat: Declare and initialize variables

--- Udemy/01_UnrealEngineC++Developer/Section2_TripleX/TripleX.cpp
+++ Udemy/01_UnrealEngineC++Developer/Section2_TripleX/TripleX.cpp
@@ -1,7 +1,21 @@
-#include <iostream> // include = preprocessor directive, <iostream> = header file
+#include <iostream>
 
 int main()
 {
-    std::cout << "Hello world!";
+    std::cout << "You are a secret agent breaking into a secure server room....";
+    std::cout << std::endl;
+    std::cout << "Enter the correct code to continue";
+
+    int a = 1;
+    int b = 2;
+    int c = 3;
+
+    int sum = a + b + c;
+    int product = a * b * c;
+
+    std::cout << std::endl;
+    std::cout << sum << std::endl;
+    std::cout << product;
+
     return 0;
 }
