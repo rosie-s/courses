54f60fdddc7e5314bfb6dee23a7a37efb7968422
rosie-s <rosie-s@users.noreply.github.com>
2019-11-24 01:04:45 +0100

    refactor: Use functions

--- Udemy/01_UnrealEngineC++Developer/Section2_TripleX/TripleX.cpp
+++ Udemy/01_UnrealEngineC++Developer/Section2_TripleX/TripleX.cpp
@@ -1,11 +1,28 @@
 #include <iostream>
 
-int main()
+void PrintIntroduction()
 {
-    // Print welcome messages to the terminal
-    std::cout << "You are a secret agent breaking into a secure server room....";
-    std::cout << std::endl;
-    std::cout << "Enter the correct code to continue" << std::endl;
+    // Print welcome messages to the terminal                                                                                    
+    std::cout << R"(
+                    .----------------.  .----------------.  .----------------.  .----------------.  .----------------.  .----------------.   .----------------. 
+                    | .--------------. || .--------------. || .--------------. || .--------------. || .--------------. || .--------------. | | .--------------. |
+                    | |  _________   | || |  _______     | || |     _____    | || |   ______     | || |   _____      | || |  _________   | | | |  ____  ____  | |
+                    | | |  _   _  |  | || | |_   __ \    | || |    |_   _|   | || |  |_   __ \   | || |  |_   _|     | || | |_   ___  |  | | | | |_  _||_  _| | |
+                    | | |_/ | | \_|  | || |   | |__) |   | || |      | |     | || |    | |__) |  | || |    | |       | || |   | |_  \_|  | | | |   \ \  / /   | |
+                    | |     | |      | || |   |  __ /    | || |      | |     | || |    |  ___/   | || |    | |   _   | || |   |  _|  _   | | | |    > `' <    | |
+                    | |    _| |_     | || |  _| |  \ \_  | || |     _| |_    | || |   _| |_      | || |   _| |__/ |  | || |  _| |___/ |  | | | |  _/ /'`\ \_  | |
+                    | |   |_____|    | || | |____| |___| | || |    |_____|   | || |  |_____|     | || |  |________|  | || | |_________|  | | | | |____||____| | |
+                    | |              | || |              | || |              | || |              | || |              | || |              | | | |              | |
+                    | '--------------' || '--------------' || '--------------' || '--------------' || '--------------' || '--------------' | | '--------------' |
+                    '----------------'  '----------------'  '----------------'  '----------------'  '----------------'  '----------------'   '----------------' 
+                    )" << std::endl; 
+    std::cout << "You are a secret agent breaking into a secure server room....\n";
+    std::cout << "Enter the correct code to continue!\n\n";
+}
+
+void PlayGame()
+{
+    PrintIntroduction();
 
     const int CodeA = 1;
     const int CodeB = 2;
@@ -15,28 +32,30 @@ int main()
     const int CodeProduct = CodeA * CodeB * CodeC;
 
     // Print sum and product to the terminal
-    std::cout << std::endl;
-    std::cout << "** There are 3 numbers in the code" << std::endl;
-    std::cout << "** The codes add up to: " << CodeSum << std::endl;
-    std::cout << "** The codes multiply to give: " << CodeProduct << std::endl;
+    std::cout << "** There are 3 numbers in the code";
+    std::cout << "\n** The codes add up to: " << CodeSum;
+    std::cout << "\n** The codes multiply to give: " << CodeProduct << std::endl;
 
+    // Store player's guess
     int GuessA, GuessB, GuessC;
-
-    std::cin >> GuessA;
-    std::cin >> GuessB;
-    std::cin >> GuessC;
+    std::cin >> GuessA >> GuessB >> GuessC;
 
     int GuessSum = GuessA + GuessB + GuessC;
     int GuessProduct = GuessA * GuessB * GuessC;
 
+    // Check it the player's guess is correct
     if (GuessSum == CodeSum && GuessProduct == CodeProduct)
     {
-        std::cout << "You Win!";
+        std::cout << "\nYou Win!";
     }
     else
     {
-        std::cout << "You Lose!";
+        std::cout << "\nYou Lose!";
     }
-    
+}
+
+int main()
+{
+    PlayGame();
     return 0;
 }
