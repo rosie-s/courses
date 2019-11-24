b08cd74f25d8438d24b9e12176dbe70d90b2080f
rosie-s <rosie-s@users.noreply.github.com>
2019-11-24 02:54:53 +0100

    refactor: Define functions and minor changes

--- Udemy/01_UnrealEngineC++Developer/Section2_TripleX/TripleX.cpp
+++ Udemy/01_UnrealEngineC++Developer/Section2_TripleX/TripleX.cpp
@@ -1,9 +1,38 @@
 #include <iostream>
 #include <ctime>
 
+// Functions definitions
+void PrintLogo();
+void PrintIntroduction(int Difficulty);
+bool PlayGame(int Difficulty, int MaxDifficulty);
+
+int main()
+{
+    srand(time(NULL)); // Create new random sequence based on the time of day
+
+    PrintLogo();
+    int LevelDifficulty = 1;
+    const int MaxDifficulty = 2;
+
+    while (LevelDifficulty <= MaxDifficulty) // Loop game until all levels are completed
+    {
+        bool bLevelComplete = PlayGame(LevelDifficulty, MaxDifficulty);
+        std::cin.clear();  // Clears any errors
+        std::cin.ignore(); // Discards the buffer
+
+        if (bLevelComplete)
+        {
+            ++LevelDifficulty;
+        }
+    }
+
+    return 0;
+}
+
+// Functions
 void PrintLogo()
 {
-    // Print logo to the terminal                                                                                    
+    // Print logo to the terminal
     std::cout << R"(
                       _______   _       _       __   __
                      |__   __| (_)     | |      \ \ / /
@@ -13,17 +42,26 @@ void PrintLogo()
                         |_|_|  |_| .__/|_|\___| /_/ \_\
                                  | |                   
                                  |_|                   
-                    )" << std::endl;
+                    )"
+              << std::endl;
+    std::cout << "\n************************************************************************";
 }
 
 void PrintIntroduction(int Difficulty)
 {
     // Print welcome messages to the terminal
-    std::cout << "\n\nYou are a secret agent breaking into a level " << Difficulty << " secure server room....\n";
+    if (Difficulty == 1)
+    {
+        std::cout << "\n\nSecret agent Agent P, you need to break into a level " << Difficulty << " secure server room....\n";
+    }
+    else
+    {
+        std::cout << "\n\nNow break into the level " << Difficulty << " secure server room....\n";
+    }
     std::cout << "Enter the correct code to continue!\n\n";
 }
 
-bool PlayGame(int Difficulty)
+bool PlayGame(int Difficulty, int MaxDifficulty)
 {
     PrintIntroduction(Difficulty);
 
@@ -49,36 +87,20 @@ bool PlayGame(int Difficulty)
     // Check it the player's guess is correct
     if (GuessSum == CodeSum && GuessProduct == CodeProduct)
     {
-        std::cout << "\n*** Well done Agent P! You have extracted a file! Keep going! ***";
-        return true;
+        if (Difficulty != MaxDifficulty)
+        {
+            std::cout << "\n*** Well done Agent P! You have extracted a file! Keep going! ***";
+            std::cout << "\n************************************************************************";
+            return true;
+        }
+        std::cout << "\n************************************************************************";
+        std::cout << "\n*** Great work Agent P! You got all the files! Now get out of there! ***";
+        std::cout << "\n************************************************************************";
     }
     else
     {
         std::cout << "\n*** You entered the wrong code! Careful Agent P! Try again! ***";
+        std::cout << "\n************************************************************************";
         return false;
     }
-}
-
-int main()
-{      
-    srand(time(NULL)); // Create new random sequence based on the time of day   
-   
-    PrintLogo();
-    int LevelDifficulty = 1;
-    const int MaxDifficulty = 5;
-
-    while(LevelDifficulty <= MaxDifficulty) // Loop game until all levels are completed
-    {
-        bool bLevelComplete = PlayGame(LevelDifficulty);
-        std::cin.clear(); // Clears any errors
-        std::cin.ignore(); // Discards the buffer
-
-        if (bLevelComplete)
-        {
-            ++LevelDifficulty;
-        }
-        
-    }
-    std::cout << "\n*** Great work Agent P! You got all the files! Now get out of there! ***";
-    return 0;
 }
