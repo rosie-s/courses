22efd616db9134ddcf85b52922bbd49baae762b1
rosie-s <rosie-s@users.noreply.github.com>
2020-05-27 17:24:11 +0200

    feat: Solution for Pattern Builder

---
+++ AJC/15.Builder/src/printer/singleton/Computer.java
@@ -0,0 +1,18 @@
+package printer.singleton;
+
+import printer.singleton.PrinterCentralizer;
+
+public class Computer {
+    public int id;
+    public String name;
+
+    public Computer(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public void print(String message) {
+        // Call of the singleton
+        PrinterCentralizer.getInstance().print(new StringBuilder(name).append(" prints ").append(message).append(" on ").toString());
+    }
+}
---
+++ AJC/15.Builder/src/printer/singleton/Printer.java
@@ -0,0 +1,14 @@
+package printer.singleton;
+
+public class Printer {
+    public int id;
+    public String name;
+
+    public  Printer(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+    public void print (String message){
+        System.out.println(message);
+    }
+}
