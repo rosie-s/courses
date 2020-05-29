dabdea3a097afc9727aa99df2ff41da7f58495d2
rosie-s <rosie-s@users.noreply.github.com>
2020-05-29 12:37:08 +0200

    feat: Create table Artist

--- AJC/03.JavaPersistenceAPI/02.Artist/src/main/java/com/ajc/models/Artist.java
+++ AJC/03.JavaPersistenceAPI/02.Artist/src/main/java/com/ajc/models/Artist.java
@@ -1,15 +1,13 @@
-package com.ajc;
+package com.ajc.models;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.Id;
-import javax.persistence.Table;
+import javax.persistence.*;
 
 @Entity
 @Table(name="artist")
 public class Artist {
     @Id
-    private  Long id;
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
 
     @Column(name ="FIRST_NAME")
     private String firstName;
