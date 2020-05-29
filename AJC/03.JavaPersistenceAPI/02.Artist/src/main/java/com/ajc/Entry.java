81ed7923655e5daf3bef7a58c7cb45ba91ad2d1b
rosie-s <rosie-s@users.noreply.github.com>
2020-05-29 14:04:57 +0200

    fix: Create table in jpatraining

--- AJC/03.JavaPersistenceAPI/02.Artist/src/main/java/com/ajc/Entry.java
+++ AJC/03.JavaPersistenceAPI/02.Artist/src/main/java/com/ajc/Entry.java
@@ -5,9 +5,6 @@ import javax.persistence.*;
 public class Entry {
     public static void main(String[] args) {
         EntityManagerFactory e = Persistence.createEntityManagerFactory("training");
-        EntityManagerFactory artist = Persistence.createEntityManagerFactory("artist");
-
         e.close();
-        artist.close();
     }
 }
