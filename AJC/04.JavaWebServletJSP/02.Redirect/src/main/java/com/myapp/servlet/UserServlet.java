d5ce46854dc77e8fcdfac7868bf231a0fd4fd721
rosie-s <rosie-s@users.noreply.github.com>
2020-06-04 13:02:05 +0200

    feat: Redirect to a different page

---
+++ AJC/04.JavaWebServletJSP/02.Redirect/src/main/java/com/myapp/servlet/UserServlet.java
@@ -0,0 +1,11 @@
+package com.myapp.servlet;
+
+import javax.servlet.annotation.WebServlet;
+import java.io.IOException;
+
+@WebServlet(value = "/userdetail")
+public class UserServlet extends javax.servlet.http.HttpServlet {
+   protected void doGet(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response) throws javax.servlet.ServletException, IOException {
+        request.getRequestDispatcher("/WEB-INF/pages/showUser.jsp").forward(request, response);
+    }
+}
