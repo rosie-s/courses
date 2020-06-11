5a460478cf84b67b241a532b549db54730efe72d
rosie-s <rosie-s@users.noreply.github.com>
2020-06-11 17:43:08 +0200

    feat: Get user list

---
+++ AJC/05.SpringEssential/07.Spring_Boot/02.SBUserProduct/src/main/java/com/app/dao/UserRepository.java
@@ -0,0 +1,10 @@
+package com.app.dao;
+
+import com.app.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    List<User> findAll();
+}
