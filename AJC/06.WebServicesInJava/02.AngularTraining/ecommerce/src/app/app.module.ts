6cd597fd9c28c20055affd74e1f62c6de871229e
rosie-s <rosie-s@users.noreply.github.com>
2020-06-12 14:54:38 +0200

    feat: Create components in ecommerce

---
+++ AJC/06.WebServicesInJava/02.AngularTraining/ecommerce/src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+import { MenuComponent } from './menu/menu.component';
+import { ProductListComponent } from './product-list/product-list.component';
+import { ProductComponent } from './product/product.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    MenuComponent,
+    ProductListComponent,
+    ProductComponent
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
