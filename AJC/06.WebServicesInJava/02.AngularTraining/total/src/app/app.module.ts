04243c3201a37930306abf4e992d03ed35b15166
rosie-s <rosie-s@users.noreply.github.com>
2020-06-16 17:07:35 +0200

    feat: Update layout using angular material

--- AJC/06.WebServicesInJava/02.AngularTraining/total/src/app/app.module.ts
+++ AJC/06.WebServicesInJava/02.AngularTraining/total/src/app/app.module.ts
@@ -1,10 +1,18 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
-import { AppComponent } from './app.component';
-import { TotalComponent } from './total/total.component';
-import { NumberComponent } from './number/number.component';
+import {AppComponent} from './app.component';
+import {TotalComponent} from './total/total.component';
+import {NumberComponent} from './number/number.component';
 import {FormsModule} from "@angular/forms";
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {MatSliderModule} from '@angular/material/slider';
+import {Component} from '@angular/core';
+import {MatCardModule} from "@angular/material/card";
+import {MatDivider, MatDividerModule} from "@angular/material/divider";
+import {MatButtonModule} from "@angular/material/button";
+import {MatToolbarModule} from '@angular/material/toolbar';
+
 
 @NgModule({
   declarations: [
@@ -12,11 +20,17 @@ import {FormsModule} from "@angular/forms";
     TotalComponent,
     NumberComponent
   ],
-    imports: [
-        BrowserModule,
-        FormsModule
-    ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    BrowserAnimationsModule,
+    MatSliderModule,
+    MatCardModule,
+    MatDividerModule, MatToolbarModule,
+    BrowserModule, FormsModule, MatButtonModule
+  ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
--- AJC/06.WebServicesInJava/02.AngularTraining/total/src/app/number/number.component.ts
+++ AJC/06.WebServicesInJava/02.AngularTraining/total/src/app/number/number.component.ts
@@ -1,27 +1,35 @@
 import {Component, EventEmitter, HostListener, Input, OnInit, Output} from '@angular/core';
+import {BrowserModule} from "@angular/platform-browser";
+import {FormsModule} from "@angular/forms";
+import {MatButtonModule} from "@angular/material/button";
 
 @Component({
-  selector: 'app-number',
-  templateUrl: './number.component.html',
-  styleUrls: ['./number.component.css']
-})
+    selector: 'app-number',
+    templateUrl: './number.component.html',
+    styleUrls: ['./number.component.css']
+  }
+)
+
 export class NumberComponent implements OnInit {
 
-  constructor() { }
+  constructor() {
+  }
+
   theInit = 0
 
   @Output()
   o = new EventEmitter<number>()
+
   ngOnInit(): void {
   }
 
   add() {
-    this.theInit =  this.theInit + 1;
+    this.theInit = this.theInit + 1;
     this.o.emit(1)
   }
 
   subtract() {
-    this.theInit =  this.theInit - 1;
+    this.theInit = this.theInit - 1;
     this.o.emit(-1)
 
   }
