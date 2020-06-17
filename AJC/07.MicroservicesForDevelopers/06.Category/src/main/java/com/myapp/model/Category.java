69021ef2bb6a37173209624f2d5f2d75a44a6610
rosie-s <rosie-s@users.noreply.github.com>
2020-06-17 15:27:07 +0200

    feat: Link product with category

---
+++ AJC/07.MicroservicesForDevelopers/06.Category/src/main/java/com/myapp/model/Category.java
@@ -0,0 +1,49 @@
+package com.myapp.model;
+
+import javax.persistence.*;
+import java.util.List;
+
+@Entity
+@Table(name = "Categories")
+public class Category {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    Long id;
+    String name;
+    String nameVN;
+
+/*    @OneToMany(mappedBy = "category", fetch = FetchType.EAGER)
+    List<Product> products;*/
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getNameVN() {
+        return nameVN;
+    }
+
+    public void setNameVN(String nameVN) {
+        this.nameVN = nameVN;
+    }
+
+/*    public List<Product> getProducts() {
+        return products;
+    }
+
+    public void setProducts(List<Product> products) {
+        this.products = products;
+    }*/
+}
