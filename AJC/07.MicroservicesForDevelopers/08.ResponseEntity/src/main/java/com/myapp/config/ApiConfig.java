e43b4d0c6f311dd1d7e77b81e6621b6fa3d42a48
rosie-s <rosie-s@users.noreply.github.com>
2020-06-17 17:06:49 +0200

    feat: Use response entity

---
+++ AJC/07.MicroservicesForDevelopers/08.ResponseEntity/src/main/java/com/myapp/config/ApiConfig.java
@@ -0,0 +1,35 @@
+package com.myapp.config;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.datatype.hibernate5.Hibernate5Module;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.util.List;
+
+@Configuration
+@EnableWebMvc
+public class ApiConfig implements WebMvcConfigurer {
+    public MappingJackson2HttpMessageConverter jsonConverter() {
+        MappingJackson2HttpMessageConverter jsonConverter = new MappingJackson2HttpMessageConverter();
+        ObjectMapper objectMapper = jsonConverter.getObjectMapper();
+        Hibernate5Module module = new Hibernate5Module();
+
+        module.disable(Hibernate5Module.Feature.USE_TRANSIENT_ANNOTATION);
+        module.disable(Hibernate5Module.Feature.FORCE_LAZY_LOADING);
+        module.disable(Hibernate5Module.Feature.WRITE_MISSING_ENTITIES_AS_NULL);
+        module.disable(Hibernate5Module.Feature.SERIALIZE_IDENTIFIER_FOR_LAZY_NOT_LOADED_OBJECTS);
+
+        objectMapper.registerModule(module);
+
+        return jsonConverter;
+    }
+
+    @Override
+    public void configureMessageConverters(List<HttpMessageConverter<?>> converters) {
+        converters.add(this.jsonConverter());
+    }
+}
